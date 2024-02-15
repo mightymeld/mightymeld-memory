e96d2a5271e858a05cbcb128041559970f0f490c
Daniel Worthington <daniel@halffullheart.com>
2024-02-15 13:25:42 -0800

    Set up MightyMeld

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const babelPlugins = [];
+
+if (process.env.MIGHTYMELD) {
+  babelPlugins.push("@mightymeld/runtime/babel-plugin-mightymeld");
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: babelPlugins,
+      },
+    }),
+  ],
+});
