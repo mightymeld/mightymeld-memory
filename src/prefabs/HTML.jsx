182a2300a15cbcdde5f331b8532d2d6dc87b7ff4
Daniel Worthington <daniel@halffullheart.com>
2024-02-19 09:16:54 -0800

    Add HTML prefabs

---
+++ src/prefabs/HTML.jsx
@@ -0,0 +1,43 @@
+export function fragment$fragment() {
+  return <></>;
+}
+
+export function div$html() {
+  return <div />;
+}
+
+export function text$text() {
+  return <div>Sample text</div>;
+}
+
+export function text_span$text() {
+  return <span>Sample text</span>;
+}
+
+export function h1$html() {
+  return <h1>Heading 1</h1>;
+}
+
+export function h2$html() {
+  return <h2>Heading 2</h2>;
+}
+
+export function h3$html() {
+  return <h3>Heading 3</h3>;
+}
+
+export function h4$html() {
+  return <h4>Heading 4</h4>;
+}
+
+export function h5$html() {
+  return <h5>Heading 5</h5>;
+}
+
+export function h6$html() {
+  return <h6>Heading 6</h6>;
+}
+
+export function img$html() {
+  return <img src="https://picsum.photos/200/300" />;
+}
